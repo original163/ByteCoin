752002aa47dd1d434f83c81144df33f8a6944e19
Denisov Denis <original163@mail.ru>
2021-06-02 16:55:57 +0400

    Реализован PickerView.

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -17,6 +17,12 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return coinManager.currencyArray[row]
     }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+        
+    }
 
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
